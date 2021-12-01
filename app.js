9aecfb219cd95e82aa0f16ffd8c2774d459d8aee
Max W <mwinter@wesleyan.edu>
2021-12-01 17:45:24 -0600

    added projects to html page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generatePage = require("./src/page-template")
+const generatePage = require("./src/page-template");
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -141,7 +141,14 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => console.log(portfolioData));
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
+    });
 
 
 /* const fs = require("fs");
