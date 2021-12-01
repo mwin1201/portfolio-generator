e290b6fa3f995664bcd14c53a0ee9f8098ff5c51
Max W <mwinter@wesleyan.edu>
2021-11-30 19:52:21 -0600

    new inquirer files and js logic

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/* const fs = require("fs");
 const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if (err) throw new Error(err);
+fs.writeFile("./index.html", pageHTML, err => {
+    if (err) throw err;
     console.log("complete, see index.html");
-});
+}); */
+
